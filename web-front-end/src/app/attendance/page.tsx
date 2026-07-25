533aafaa7f9a8aee37c699bee589847681fd265d
Damsi Jayawarna <damsijayawarna7@gmail.com>
2026-07-25 15:42:13 +0530

    refactor: remove unused attendance and teacher attendance page components

--- web-front-end/src/app/attendance/page.tsx
+++
@@ -1,5 +0,0 @@
-import AttendanceManagementPage from '@/features/attendance/AttendanceManagementPage'
-
-export default function AdminAttendancePage() {
-  return <AttendanceManagementPage />
-}
--- web-front-end/src/app/teacher-attendance/page.tsx
+++
@@ -1,5 +0,0 @@
-import TeacherAttendancePage from '@/features/attendance/TeacherAttendancePage'
-
-export default function TeacherMarkAttendancePage() {
-  return <TeacherAttendancePage />
-}
