408479766ff640917ba3e864962bd2e6eea88e9e
Damsi Jayawarna <damsijayawarna7@gmail.com>
2026-07-27 12:05:52 +0530

    Update .gitignore, add keycloak volume in docker-compose, remove unused docker-compose file, and configure Babel for Expo

--- mobile-front-end/metro.config.js
+++ mobile-front-end/metro.config.js
@@ -2,4 +2,8 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
+// Fix for Windows: prevent Metro from trying to create directories with colons
+// in their names (like node:sea, node:crypto, etc.) which are invalid on Windows
+config.resolver.nodeExternalsPattern = /^(?!.*:).+$/;
+
 module.exports = config;
